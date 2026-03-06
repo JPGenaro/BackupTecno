e787c7628118fce79b9f846fcbcacbe4b9b3e0df
JPGenaro <jpgenaro06@gmail.com>
2026-03-06 20:47:05 -0300

    .

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://www.backuptecno.com.ar';
+  const baseUrl = 'www.backuptecno.com.ar';
 
   return {
     rules: {
